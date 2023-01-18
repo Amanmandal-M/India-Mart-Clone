0bf16df7200dfc808c6ace79b85ddacd765aec41
Aman Kumar <amanmandal644@gmail.com>
2023-01-18 14:43:45 +0530

    Writing Code to listening

---
+++ Backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { connection } = require('./Config/db');
+
+require('dotenv').config();
+
+const app = express();
+app.use(express.json());
+
+// app.use()
+
+
+app.listen(process.env.PORT, async ()=>{
+    try {
+        await connection
+        console.log(`Connected to Database`);
+        console.log(`PORT is running at ${process.env.PORT}`);
+    } catch (error) {
+        console.log(`Error in Listening : ${error}`);
+    }
+})
