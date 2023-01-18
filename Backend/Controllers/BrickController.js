418f54c1ed8305840a4ae23bb17fa1ac6b1b1b72
Aman Kumar <amanmandal644@gmail.com>
2023-01-18 22:37:10 +0530

    Working on Routers

--- Backend/Controllers/BrickController.js
+++ Backend/Controllers/BrickController.js
@@ -20,7 +20,9 @@ const PostBrick = async (req, res) => {
     try {
         const brick = new BrickModel(req.body);
         await brick.save();
-        res.send("Product Added Successfully");
+        res.send({
+            "Message":"Product Added Successfully"
+        });
     } catch (error) {
         console.log(`Error in PostBrick : ${error}`);
         res.send({
@@ -29,5 +31,35 @@ const PostBrick = async (req, res) => {
     }
 }
 
+const UpdateBrick = async (req, res) => {
+    const ID = req.params.id;
+    const payload = req.body;
+    try {
+        const data = await BrickModel.findByIdAndUpdate({_id:ID}, payload)
+        res.send({
+            "Message":"Data updated successfully"
+        })
+    } catch (error) {
+        console.log(`Error in UpdateBrick : ${error}`);
+        res.send({
+            "Message": "Error in UpdateBrick : ${error}",
+        })
+    }
+}
 
-module.exports = {GetBrick,PostBrick}
+
+const DeleteBrick = async (req, res) => {
+    const ID = req.params.id;
+    try {
+        const data = await BrickModel.findByIdAndDelete({_id:ID})
+        res.send({
+            "Message":"Data Deleted successfully"
+        })
+    } catch (error) {
+        console.log(`Error in DeleteBrick : ${error}`);
+        res.send({
+            "Message": "Error in DeleteBrick : ${error}",
+        })
+    }
+}
+module.exports = {GetBrick,PostBrick,UpdateBrick,DeleteBrick}
