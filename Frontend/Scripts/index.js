5e7ae03bd2e4a2138cf727fb85f78f3a38348562
Aman Kumar <amanmandal644@gmail.com>
2023-01-21 23:30:22 +0530

    Pending

--- Frontend/Scripts/index.js
+++ Frontend/Scripts/index.js
@@ -1,3 +1,7 @@
 document.querySelector("#sign").addEventListener('click',(e)=>{
     location.pathname = '/Frontend/Views/signup.html';
 })
+
+document.querySelector("#admin").addEventListener('click',(e)=>{
+    location.pathname = '/Frontend/Views/adminLogin.html';
+})
